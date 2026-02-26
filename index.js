3ba51e6cb180410c51b756385df1a8b66acd1be1
Aremanky <adrlopgam@alum.us.es>
2026-02-26 20:22:09 +0100

    Limpieza de codigo y hacer pagina statica

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const cool = require('cool-ascii-faces');
 const app = express();
-const port = process.env.PORT || 8080; // Vital para que Render asigne el puerto correctamente
+const port = process.env.PORT || 8080; 
+
+app.use('/', express.static('public'));
 
 app.get('/cool', (req, res) => {
     res.send(cool());
-});
-
-const path = require('path');
-
-// Sirve la carpeta public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Enlaza la ruta /about directamente al html
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(port, () => {
