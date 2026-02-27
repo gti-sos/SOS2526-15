a8287ebb20a0a6c8f8af8eac0d2dcd948fcece20
Aremanky <adrlopgam@alum.us.es>
2026-02-27 13:24:02 +0100

    Arreglao ejercicio

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ app.listen(port, () => {
 
 // --- Datos y algoritmo de YHX ---
 const datosDemograficos = [
-    { country: "España", year: 2025, density: 98, population: 49337356, percentage_change: 0.49 },
-    { country: "España", year: 2024, density: 97, population: 49096877, percentage_change: 0.59 },
-    { country: "España", year: 2023, density: 96, population: 48628256, percentage_change: 0.51 },
-    { country: "España", year: 2022, density: 95, population: 48047631, percentage_change: 0.53 },
-    { country: "Alemania", year: 2022, density: 232, population: 83118501, percentage_change: -0.14 },
-    { country: "Alemania", year: 2023, density: 233, population: 83456045, percentage_change: 0.41 },
-    { country: "Alemania", year: 2024, density: 234, population: 83577140, percentage_change: 0.15 },
-    { country: "Alemania", year: 2025, density: 234, population: 83577140, percentage_change: 0.15 },
-    { country: "Ucrania", year: 2023, density: 63, population: 37732836, percentage_change: -8.08 },
-    { country: "Ucrania", year: 2022, density: 68, population: 41048766, percentage_change: 0.12 }
+    { country: "españa", year: 2025, density: 98, population: 49337356, percentage_change: 0.49 },
+    { country: "españa", year: 2024, density: 97, population: 49096877, percentage_change: 0.59 },
+    { country: "españa", year: 2023, density: 96, population: 48628256, percentage_change: 0.51 },
+    { country: "españa", year: 2022, density: 95, population: 48047631, percentage_change: 0.53 },
+    { country: "alemania", year: 2022, density: 232, population: 83118501, percentage_change: -0.14 },
+    { country: "alemania", year: 2023, density: 233, population: 83456045, percentage_change: 0.41 },
+    { country: "alemania", year: 2024, density: 234, population: 83577140, percentage_change: 0.15 },
+    { country: "alemania", year: 2025, density: 234, population: 83577140, percentage_change: 0.15 },
+    { country: "ucrania", year: 2023, density: 63, population: 37732836, percentage_change: -8.08 },
+    { country: "ucrania", year: 2022, density: 68, population: 41048766, percentage_change: 0.12 }
 ];
 
 function mediaPoblacion(pais) {
@@ -48,7 +48,7 @@ function mediaPoblacion(pais) {
 
 app.get('/samples/YHX', (req, res) => {
     // Calculamos la media llamando a tu función
-    const media = mediaPoblacion("España");
+    const media = mediaPoblacion("españa");
     
     // Enviamos el resultado al navegador web
     res.send(`Media de población en España: ${media.toLocaleString('es-ES')}`);
@@ -57,9 +57,13 @@ app.get('/samples/YHX', (req, res) => {
 // -------------------------------------------------
 
 app.get(`${API_URL}/loadInitialData`, (req, res) => {
-
-    stats = datosDemograficos
-    res.status(201).json(stats); // 201: Creado
+    if (stats.length === 0) {
+        // Hacemos una copia de los datos originales
+        stats = [datosDemograficos]; 
+        res.status(201).json(stats);
+    } else {
+        res.status(409).json({ message: "Los datos ya estaban cargados" });
+    }
 });
 
 // GET: Devuelve toda la lista
