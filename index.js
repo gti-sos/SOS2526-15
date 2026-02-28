fe61b10923ca0b904e46bba97f9a83fe2145c32c
unknown <alemonbae1@alum.us.es>
2026-02-28 10:10:30 +0100

    Index añadido de SMB 1

--- index.js
+++ index.js
@@ -56,6 +56,39 @@ app.get('/samples/YHX', (req, res) => {
 
 // -------------------------------------------------
 
+//--- Datos y algoritmo de SMB ---
+const datosNmw = [
+    { country: "iran", date: 2024, national_currency_minimum_wage: 53073300.00, nmw_on_dollar: 1263.70, percentage_change: -6.04 },
+    { country: "united states", date: 2026, national_currency_minimum_wage: 1257.00, nmw_on_dollar: 1257.00, percentage_change: -3.48 },
+    { country: "united states", date: 2025, national_currency_minimum_wage: 1257.00, nmw_on_dollar: 1257.00, percentage_change: -8.38 },
+    { country: "united states", date: 2024, national_currency_minimum_wage: 1257.00, nmw_on_dollar: 1257.00, percentage_change: 1.85 },
+    { country: "iran", date: 2022, national_currency_minimum_wage: 26554950.00, nmw_on_dollar: 632.30, percentage_change: 126.70 },
+    { country: "spain", date: 2026, national_currency_minimum_wage: 1381.00, nmw_on_dollar: 1622.70, percentage_change: 0 },
+    { country: "spain", date: 2025, national_currency_minimum_wage: 1381.00, nmw_on_dollar: 1435.00, percentage_change: -0.02 },
+    { country: "spain", date: 2024, national_currency_minimum_wage: 1381.00, nmw_on_dollar: 1461.90, percentage_change: 0 },
+    { country: "canada", date: 2025, national_currency_minimum_wage: 2872.10, nmw_on_dollar: 1994.50, percentage_change: 0.81 },
+    { country: "portugal", date: 2026, national_currency_minimum_wage: 1073.00, nmw_on_dollar: 1260.80, percentage_change: 5.71 }
+];
+
+function mediaSalario(pais){
+    let subconjunto = datosNmw.filter(d => d.country === pais);
+    let valores = subconjunto.map(d => d.nmw_on_dollar);
+    let suma = 0;
+    valores.forEach(valor => {
+        suma += valor;
+    });
+    let media = valores.length > 0 ? suma / valores.length : 0;
+    return media;
+}
+
+app.get('/samples/SMB', (req, res) => {
+
+    const media = mediaSalario("spain");
+
+    res.send(`Media del salario mínimo en dólares en España: ${media.toLocaleString('es-ES')}`);
+});
+// -------------------------------------------------
+
 app.get(`${API_URL}/loadInitialData`, (req, res) => {
     if (stats.length === 0) {
         // Hacemos una copia de los datos originales
