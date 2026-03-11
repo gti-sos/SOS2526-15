d651c2312eca3fea70b3efef31aa25726500cbe6
Javier Arroyo <javarrmar.alum.us.es>
2026-03-11 09:28:36 +0100

    Añadido GET de mi API con NeDB, paginación, búsquedas y sin el campo _id

--- index-JAM.js
+++ index-JAM.js
@@ -34,3 +34,34 @@ if (filteredData.length === 0) {
     // 5. Mostramos el resultado
     console.log(`La media de happiness_score para ${filterCountry} es: ${averageScore.toFixed(3)}`);
 }
+
+
+// Lo que he añadido en L06
+app.get('/api/v1/happiness-indices', (req, res) => {
+    // 1. Preparar la paginación (si el usuario no manda limit/offset, por defecto es 0)
+    const limit = req.query.limit ? parseInt(req.query.limit) : 0; 
+    const offset = req.query.offset ? parseInt(req.query.offset) : 0;
+
+    // 2. Preparar la búsqueda por campos
+    // Hacemos una copia de los parámetros de la URL para buscar
+    const searchQuery = { ...req.query }; 
+    // Borramos limit y offset de la búsqueda para que NeDB no intente buscar un recurso que tenga el campo "limit"
+    delete searchQuery.limit;
+    delete searchQuery.offset;
+
+    // 3. Buscar en NeDB aplicando paginación
+    db.find(searchQuery).skip(offset).limit(limit).exec((err, docs) => {
+        if (err) {
+            console.error("Error al acceder a la base de datos", err);
+            res.sendStatus(500); 
+        } else {
+            // 4. Limpiar el _id autogenerado (requisito de la rúbrica)
+            docs.forEach(doc => {
+                delete doc._id;
+            });
+            
+            // 5. Devolver siempre un Array en JSON con código 200 OK
+            res.status(200).json(docs);
+        }
+    });
+});
