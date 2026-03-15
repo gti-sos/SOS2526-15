71407cbf921062b0ad681af8bb8027067be48f78
Aremanky <adrlopgam@alum.us.es>
2026-03-15 19:21:45 +0100

    Corregido

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8080; 
 import {loadBackendSMB} from './src/routes/SMB.js';
 import {loadBackendYHX} from './src/routes/YHX.js';
-import {loadBackendYHX} from './src/routes/JAM.js';
+import {loadBackendJAM} from './src/routes/JAM.js';
 
 app.use('/', express.static('public'));
 
