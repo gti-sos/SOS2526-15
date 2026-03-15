9b54cf337ecbb44445290152c3d12a374ebd0f57
Aremanky <adrlopgam@alum.us.es>
2026-03-15 12:02:52 +0100

    Cool activo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cool from 'cool-ascii-faces';
 const app = express();
 const port = process.env.PORT || 8080; 
 import {loadBackendSMB} from './src/routes/SMB.js';
