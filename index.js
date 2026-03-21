23d569bf0d9ebab85eee9a6045b86d5b3fcf0bb8
Samanta Montoya Baena <alemonbae1@alum.us.es>
2026-03-21 12:37:36 +0100

    arreglos al test

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ util.isRegExp = util.types.isRegExp;
 util.isError = util.types.isNativeError;
 
 import express from 'express';
-import cool from 'cool-ascii-faces';
+
 
 const app = express();
 const port = process.env.PORT || 8080; 
 import {loadBackendSMB} from './src/routes/SMB.js';
 import {loadBackendYHX} from './src/routes/YHX.js';
 import {loadBackendJAM} from './src/routes/JAM.js';
-import {loadBackendJAM} from './src/routes/JAMv2.js';
+//import {loadBackendJAM} from './src/routes/JAMv2.js';
 
 app.use('/', express.static('public'));
 
