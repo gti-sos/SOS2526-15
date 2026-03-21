159a9c5c4a13e961502b8550a90927065d27b230
Samanta Montoya Baena <alemonbae1@alum.us.es>
2026-03-21 16:53:28 +0100

    mergeo

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors());
 const port = process.env.PORT || 8080; 
 import {loadBackendSMB} from './src/routes/SMB.js';
 import {loadBackendYHX} from './src/routes/YHX.js';
-import {loadBackendJAM} from './src/routes/JAM.js';
-//import {loadBackendJAM} from './src/routes/JAMv2.js';
+//import {loadBackendJAM} from './src/routes/JAM.js';
+import {loadBackendJAM} from './src/routes/JAMv2.js';
 
 //app.use('/', express.static('public'));
 
