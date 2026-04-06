9a9a2d95acba2f89cb98c30cc27a42677934fb46
Javier Arroyo <javarrmar.alum.us.es>
2026-04-06 09:57:56 +0200

    feat: buscador final por url y arreglos

--- frontend/src/routes/happiness-indices/+page.svelte
+++ frontend/src/routes/happiness-indices/+page.svelte
@@ -152,8 +152,8 @@ async function buscarDatos() {
         <input type="number" placeholder="Puntuación"       bind:value={searchScore}  step="0.001"/>
         <input type="number" placeholder="PIB per cápita"   bind:value={searchGdp}    step="0.001"/>
         <input type="number" placeholder="Soporte Social"   bind:value={searchSocial} step="0.001"/>
-        <input type="number" placeholder="Límite"           bind:value={searchLimit} />
-        <input type="number" placeholder="Offset"           bind:value={searchOffset} />
+        <input type="number" placeholder="Desde el año..."           bind:value={searchLimit} />
+        <input type="number" placeholder="Hasta el año..."           bind:value={searchOffset} />
         <button onclick={buscarDatos}>Buscar</button>
         <button onclick={limpiarBusqueda}>Limpiar</button>
     </div>
